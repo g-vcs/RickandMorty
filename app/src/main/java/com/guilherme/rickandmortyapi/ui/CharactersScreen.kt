c75161205337c32c63d5391c6a3757314b176cf0
Guilherme Sousa <guilhermevcsousa@hotmail.com>
2024-02-25 19:16:53 +0000

    trying to add image as header and make it scroolable

--- app/src/main/java/com/guilherme/rickandmortyapi/ui/CharactersScreen.kt
+++ app/src/main/java/com/guilherme/rickandmortyapi/ui/CharactersScreen.kt
@@ -1,7 +1,9 @@
 package com.guilherme.rickandmortyapi.ui
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -9,9 +11,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +26,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -30,48 +36,67 @@ import com.guilherme.rickandmortyapi.model.Character
 import com.guilherme.rickandmortyapi.viewmodel.CharacterViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.AsyncImage
+import com.guilherme.rickandmortyapi.R
 
 
 @Composable
 fun CharactersScreen(viewModel: CharacterViewModel = viewModel(), navController: NavController) {
     val context = LocalContext.current
-    val characterPagingItems: LazyPagingItems<Character> = viewModel.characterItem.collectAsLazyPagingItems()
+    val characterPagingItems: LazyPagingItems<Character> =
+        viewModel.characterItem.collectAsLazyPagingItems()
 
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        contentPadding = PaddingValues(16.dp),
-        modifier = Modifier.fillMaxSize()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
     ) {
-        items(characterPagingItems.itemCount) { index ->
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
+
+            Image(
+                painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "",
+                contentScale = ContentScale.FillBounds,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(8.dp)
+            )
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                contentPadding = PaddingValues(16.dp),
+                modifier = Modifier.fillMaxSize()
+
+
             ) {
-                Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier
-                        .size(120.dp)
-                        .clip(CircleShape)
-                        .clickable { navController.navigate(Destination.charInfoScreen.route) }
-                ) {
-                    AsyncImage(
-                        model = characterPagingItems[index]?.image,
-                        contentDescription = null,
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier.fillMaxSize()
-                    )
+                items(characterPagingItems.itemCount) { index ->
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    ) {
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier
+                                .size(120.dp)
+                                .clip(CircleShape)
+                                .clickable { navController.navigate(Destination.charInfoScreen.route) }
+                        ) {
+                            AsyncImage(
+                                model = characterPagingItems[index]?.image,
+                                contentDescription = null,
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier.fillMaxSize()
+                            )
+                        }
+                        Text(
+                            text = characterPagingItems[index]?.name ?: "Unknown",
+                            color = Color.Black,
+                            style = MaterialTheme.typography.body1,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.padding(top = 8.dp)
+                        )
+                    }
                 }
-                Text(
-                    text = characterPagingItems[index]?.name ?: "Unknown",
-                    color = Color.Black,
-                    style = MaterialTheme.typography.body1,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.padding(top = 8.dp)
-                )
             }
-        }
     }
 }
 
